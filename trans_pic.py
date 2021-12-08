95d493a76d92d2a1cb2e48b20f401cffafdb46e5
wansit99 <wansit99@gmail.com>
2021-12-08 16:38:51 +0800

    fix:修改了一些代码

--- trans_pic.py
+++ trans_pic.py
@@ -4,25 +4,21 @@ import sys
 from PIL import Image
 from shutil import copy
 
-input_folder = r'D:\软考\数据集\1'  # 源文件夹，包含.png格式图片
-output_folder = r'D:\软考\数据集\JPEGImages'   # 输出文件夹
-# training_data=[]
+input_folder = r''  # 源文件夹，包含.png格式图片
+output_folder = r''   # 输出文件夹
+
 a = []
 b = []
 for root, dirs, files in os.walk(input_folder):
     for filename in (x for x in files):
         if filename.endswith('.jpg'):
             filepath = os.path.join(root, filename)
-
             object_class = filename
             b.append(object_class)
-            #print(a)
         else:
             filepath = os.path.join(root, filename)
             object_class = filename
             a.append(object_class)
-            #print(b)
-
 
 for i in a:
     old_path = input_folder + "\\" + str(i)
